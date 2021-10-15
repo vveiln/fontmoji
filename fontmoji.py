4eb57c8311c8d1f7808716238ce4f4a6b885f967
Yulia Khalniyazova <yulia@heliax.dev>
2021-10-15 22:55:23 +0700

    Add concatenation

--- fontmoji.py
+++ fontmoji.py
@@ -6,29 +6,15 @@ pretzel = u"\U0001F968"
 emoji = pretzel
 
 
-
-def i():
-    size = (3, 5)
-    offset = 0
-    cfg = [0b111, 0b010, 0b010, 0b010, 0b111]
-    
-
-def ap():
-    size = (1, 2)
-    offset = 0
-    cfg = [0b1, 0b1]
-
-def comma():
-    size = (1, 2)
-    offset = 5
-    cfg = [0b1, 0b1]
-
-
-def emojize(img, cfg, fnt):
-    draw = ImageDraw.Draw(img)
+def emojize(cfg, fnt):
     size = cfg["size"]
     offset = cfg["offset"]
     c = cfg["c"]
+    
+    width = (109 + 27) * size[0]
+    height = 109 * (1 + 5 + 1) + 20 * 6
+    img = Image.new("RGBA", (width, height), (256, 256, 256, 256))
+    draw = ImageDraw.Draw(img)
     
     for i in range(size[1]):
         line = ""
@@ -38,8 +24,17 @@ def emojize(img, cfg, fnt):
                 line += emoji
             else:
                 line += " "
-        draw.text((0, 32 + (109 + 20) * i), line, fill = (256, 256, 256), embedded_color=True, font = fnt)
-    img.save("out1.png")
+        draw.text((0, 109 + (109 + 20) * i), line, fill = (256, 256, 256), embedded_color=True, font = fnt)
+    return img
+
+def concat(imgs):
+    width = sum([im.width for im in imgs])
+    dst = Image.new('RGB', (width, imgs[0].height))
+    w = 0
+    for i in imgs:
+        dst.paste(i, (w, 0))
+        w += i.width
+    return dst
 
 if __name__ == "__main__":
     font = "./NotoColorEmoji.ttf"
@@ -48,17 +43,15 @@ if __name__ == "__main__":
 
     fnt = ImageFont.truetype(font, size=size, layout_engine=ImageFont.LAYOUT_RAQM)
     im = Image.new("RGBA", (800, 700), (256, 256, 256, 256))
-    draw = ImageDraw.Draw(im)
 
-    cfg = {"size": (3,5), "offset": 0, "c": ["xxx", "qxq", "qxq", "qxq", "xxx"]}
-    cfg = {"size": (5,5), "offset": 0, "c": ["xqqqx", "xxqxx", "xqxqx", "xqqqx", "xqqqx"]}
+
+    cfg1 = {"size": (3,5), "offset": 0, "c": ["xxx", "qxq", "qxq", "qxq", "xxx"]}
+    cfg2 = {"size": (5,5), "offset": 0, "c": ["xqqqx", "xxqxx", "xqxqx", "xqqqx", "xqqqx"]}
+    cfga = {"size": (5,5), "offset": 0, "c": ["qqxqq", "qxqxq", "qxqxq", "xxxxx", "xqqqx"]}
     #cfg = {"size":(1, 2), "offset": 0, "c": ["x", "x"]}
-    emojize(im, cfg, fnt)
-
-#    draw.text((0, 32), emoji * 3, fill = (256, 256, 256), embedded_color=True, font=fnt)
-#    draw.text((size + space, 32 + size + 20), emoji, fill = (256, 256, 256), embedded_color=True, font=fnt)
-#    draw.text((size + space, 32 + (size + 20) * 2), emoji, fill = (256, 256, 256), embedded_color=True, font=fnt)
-#    draw.text((size + space, 32 + (size + 20) * 3), emoji, fill = (256, 256, 256), embedded_color=True, font=fnt)
-#    draw.text((0, 32 + (size + 20) * 4), emoji * 3, fill = (256, 256, 256), embedded_color=True, font=fnt)
-#
-#    im.save("./out.png")
+    buf = Image.new("RGBA", (50, (109 * 7 + 20 * 6)), (256, 256, 256, 256))
+    img1 = emojize(cfg1, fnt)
+    img2 = emojize(cfg2, fnt)
+    imga = emojize(cfga, fnt)
+    
+    concat([img1, buf, img2, buf, img2]).save("out.png")
